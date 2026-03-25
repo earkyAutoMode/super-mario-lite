744e3d529d426234ab2bf58e1cefddab5424b33a
earkyAutoMode <496519969@qq.com>
2026-03-25 15:13:45 +0800

    feat: 替换为原版像素精灵图并适配动画

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // 游戏常量配置
 const CONFIG = {
     canvasWidth: 800,
@@ -9,10 +8,10 @@ const CONFIG = {
     moveSpeed: 5,
     tileWidth: 40,
     tileHeight: 40,
-    // 资源配置
+    spriteSize: 16, // 原版像素大小
     assets: {
-        // 使用一个包含全资源的精灵图链接（常用的马里奥 1-1 资产图）
-        sprites: 'https://raw.githubusercontent.com/yemreak/Super-Mario-Bros/master/img/sprites.png'
+        tiles: 'https://raw.githubusercontent.com/meth-meth-method/super-mario/master/public/img/tiles.png',
+        sprites: 'https://raw.githubusercontent.com/meth-meth-method/super-mario/master/public/img/sprites.png'
     }
 };
 
@@ -25,38 +24,38 @@ const overlay = document.getElementById('overlay');
 canvas.width = CONFIG.canvasWidth;
 canvas.height = CONFIG.canvasHeight;
 
-// 图像资源加载
-const spriteSheet = new Image();
-spriteSheet.src = 'https://raw.githubusercontent.com/meth-meth-method/super-mario/master/public/img/tiles.png';
-const charSheet = new Image();
-charSheet.src = 'https://raw.githubusercontent.com/meth-meth-method/super-mario/master/public/img/characters.png';
-
-// 精灵图切片坐标映射 (基于常用的 NES 16x16 规格)
-const SPRITE_MAP = {
-    ground: { x: 0, y: 0, w: 16, h: 16 },
-    brick: { x: 16, y: 0, w: 16, h: 16 },
-    question: { x: 384, y: 0, w: 16, h: 16 },
-    pipe_top_left: { x: 0, y: 128, w: 16, h: 16 },
-    pipe_top_right: { x: 16, y: 128, w: 16, h: 16 },
-    pipe_left: { x: 0, y: 144, w: 16, h: 16 },
-    pipe_right: { x: 16, y: 144, w: 16, h: 16 },
-    mario_idle: { x: 276, y: 44, w: 16, h: 16 },
-    mario_run1: { x: 290, y: 44, w: 16, h: 16 },
-    mario_run2: { x: 304, y: 44, w: 16, h: 16 },
-    mario_jump: { x: 355, y: 44, w: 16, h: 16 },
-    goomba1: { x: 0, y: 16, w: 16, h: 16 },
-    goomba2: { x: 16, y: 16, w: 16, h: 16 },
-    coin: { x: 384, y: 0, w: 16, h: 16 }
-};
+// 禁用图像平滑以保持像素风
+ctx.imageSmoothingEnabled = false;
 
 let score = 0;
 let gameOver = false;
 let gameWon = false;
 let cameraX = 0;
-let frameCount = 0;
+
+// 资源加载
+const images = {};
+let assetsLoaded = 0;
+const totalAssets = 2;
+
+function loadAssets(callback) {
+    const assetKeys = Object.keys(CONFIG.assets);
+    assetKeys.forEach(key => {
+        const img = new Image();
+        img.src = CONFIG.assets[key];
+        img.onload = () => {
+            assetsLoaded++;
+            if (assetsLoaded === totalAssets) callback();
+        };
+        images[key] = img;
+    });
+}
 
 // 键盘控制
-const keys = { right: false, left: false, up: false };
+const keys = {
+    right: false,
+    left: false,
+    up: false
+};
 
 window.addEventListener('keydown', (e) => {
     if (e.code === 'KeyD' || e.code === 'ArrowRight') keys.right = true;
@@ -70,46 +69,62 @@ window.addEventListener('keyup', (e) => {
     if (e.code === 'KeyW' || e.code === 'ArrowUp' || e.code === 'Space') keys.up = false;
 });
 
-// 游戏基础物理对象
+// 精灵坐标配置 (基于 meth-meth-method 的配置)
+const SPRITES = {
+    player: {
+        idle: [0, 88],
+        run1: [16, 88],
+        run2: [32, 88],
+        run3: [48, 88],
+        jump: [80, 88]
+    },
+    enemy: {
+        walk1: [0, 16], // 常见的板栗仔坐标 (推测)
+        walk2: [16, 16],
+        flat: [32, 16]
+    },
+    tiles: {
+        ground: [0, 0],
+        brick: [1, 0],
+        pipeTL: [0, 5],
+        pipeTR: [1, 5],
+        pipeBL: [0, 6],
+        pipeBR: [1, 6],
+        coin: [15, 0]
+    }
+};
+
+// 基础物理对象
 class GameObject {
-    constructor(x, y, w, h, type) {
+    constructor(x, y, w, h) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.type = type;
         this.vx = 0;
         this.vy = 0;
-    }
-    draw() {
-        let s = SPRITE_MAP[this.type];
-        if (s) {
-            ctx.drawImage(spriteSheet, s.x, s.y, s.w, s.h, this.x - cameraX, this.y, this.w, this.h);
-        } else {
-            // 回退方案
-            ctx.fillStyle = '#8b4513';
-            ctx.fillRect(this.x - cameraX, this.y, this.w, this.h);
-        }
     }
 }
 
 // 玩家类
 class Player extends GameObject {
     constructor() {
-        super(100, 300, 32, 32, 'mario_idle');
+        super(100, 300, 32, 32);
         this.grounded = false;
-        this.facingRight = true;
+        this.facing = 1; // 1: 右, -1: 左
         this.animFrame = 0;
+        this.animTimer = 0;
     }
+
     update() {
         if (keys.right) {
             this.vx += 1;
-            this.facingRight = true;
+            this.facing = 1;
         } else if (keys.left) {
             this.vx -= 1;
-            this.facingRight = false;
+            this.facing = -1;
         }
-        
+
         this.vx *= CONFIG.friction;
         if (Math.abs(this.vx) < 0.1) this.vx = 0;
         this.vx = Math.max(-CONFIG.moveSpeed, Math.min(CONFIG.moveSpeed, this.vx));
@@ -119,74 +134,126 @@ class Player extends GameObject {
             this.vy = CONFIG.jumpStrength;
             this.grounded = false;
         }
-        
+
         this.vy += CONFIG.gravity;
         this.y += this.vy;
+
         this.grounded = false;
 
-        if (this.x < 0) { this.x = 0; this.vx = 0; }
-        if (this.y > CONFIG.canvasHeight) endGame('游戏结束');
+        if (this.x < 0) {
+            this.x = 0;
+            this.vx = 0;
+        }
+
+        if (this.y > CONFIG.canvasHeight) {
+            endGame('游戏结束');
+        }
 
         // 动画逻辑
         if (!this.grounded) {
-            this.type = 'mario_jump';
+            this.state = 'jump';
         } else if (this.vx !== 0) {
-            this.animFrame++;
-            this.type = (Math.floor(this.animFrame / 10) % 2 === 0) ? 'mario_run1' : 'mario_run2';
+            this.state = 'run';
+            this.animTimer++;
+            if (this.animTimer > 5) {
+                this.animFrame = (this.animFrame + 1) % 3;
+                this.animTimer = 0;
+            }
         } else {
-            this.type = 'mario_idle';
+            this.state = 'idle';
+            this.animFrame = 0;
         }
     }
+
     draw() {
-        let s = SPRITE_MAP[this.type] || SPRITE_MAP['mario_idle'];
+        let coords;
+        if (this.state === 'jump') {
+            coords = SPRITES.player.jump;
+        } else if (this.state === 'run') {
+            const frames = [SPRITES.player.run1, SPRITES.player.run2, SPRITES.player.run3];
+            coords = frames[this.animFrame];
+        } else {
+            coords = SPRITES.player.idle;
+        }
+
         ctx.save();
-        if (!this.facingRight) {
+        if (this.facing === -1) {
             ctx.translate(this.x - cameraX + this.w, this.y);
             ctx.scale(-1, 1);
-            ctx.drawImage(charSheet, s.x, s.y, s.w, s.h, 0, 0, this.w, this.h);
+            ctx.drawImage(images.sprites, coords[0], coords[1], 16, 16, 0, 0, this.w, this.h);
         } else {
-            ctx.drawImage(charSheet, s.x, s.y, s.w, s.h, this.x - cameraX, this.y, this.w, this.h);
+            ctx.drawImage(images.sprites, coords[0], coords[1], 16, 16, this.x - cameraX, this.y, this.w, this.h);
         }
         ctx.restore();
     }
 }
 
-// 敌人类
+// 敌人
 class Enemy extends GameObject {
     constructor(x, y) {
-        super(x, y, 32, 32, 'goomba1');
+        super(x, y, 32, 32);
         this.dir = -1;
         this.speed = 1.5;
         this.animFrame = 0;
+        this.animTimer = 0;
     }
+
     update() {
         this.x += this.dir * this.speed;
-        this.animFrame++;
-        this.type = (Math.floor(this.animFrame / 15) % 2 === 0) ? 'goomba1' : 'goomba2';
-        
-        // 简单碰撞检测让怪物在平台边缘折返（此处简化，仅做基础位移）
-        platforms.forEach(p => {
-            if (this.x < p.x && this.dir === -1 && this.y + this.h > p.y) this.dir = 1;
-            if (this.x + this.w > p.x + p.w && this.dir === 1 && this.y + this.h > p.y) this.dir = -1;
-        });
+        this.animTimer++;
+        if (this.animTimer > 10) {
+            this.animFrame = (this.animFrame + 1) % 2;
+            this.animTimer = 0;
+        }
     }
+
     draw() {
-        let s = SPRITE_MAP[this.type];
-        ctx.drawImage(charSheet, s.x, s.y, s.w, s.h, this.x - cameraX, this.y, this.w, this.h);
+        const coords = this.animFrame === 0 ? SPRITES.enemy.walk1 : SPRITES.enemy.walk2;
+        ctx.drawImage(images.sprites, coords[0], coords[1], 16, 16, this.x - cameraX, this.y, this.w, this.h);
     }
 }
 
-// 金币类
+// 瓦片/平台
+class Tile extends GameObject {
+    constructor(x, y, w, h, type) {
+        super(x, y, w, h);
+        this.type = type;
+    }
+
+    draw() {
+        let coords = SPRITES.tiles[this.type] || SPRITES.tiles.ground;
+        
+        // 如果是水管，需要特殊处理铺满（由于目前逻辑是长方形物体，我们简单平铺）
+        const cols = Math.ceil(this.w / 40);
+        const rows = Math.ceil(this.h / 40);
+
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                let currentCoords = coords;
+                if (this.type === 'pipe') {
+                    if (i === 0 && j === 0) currentCoords = SPRITES.tiles.pipeTL;
+                    else if (i === 1 && j === 0) currentCoords = SPRITES.tiles.pipeTR;
+                    else if (i === 0) currentCoords = SPRITES.tiles.pipeBL;
+                    else currentCoords = SPRITES.tiles.pipeBR;
+                }
+                ctx.drawImage(images.tiles, currentCoords[0] * 16, currentCoords[1] * 16, 16, 16, 
+                    this.x - cameraX + i * 40, this.y + j * 40, 40, 40);
+            }
+        }
+    }
+}
+
+// 金币
 class Coin extends GameObject {
     constructor(x, y) {
-        super(x, y, 24, 24, 'coin');
+        super(x, y, 24, 24);
         this.collected = false;
     }
+
     draw() {
-        if (!this.collected) {
-            let s = SPRITE_MAP['coin'];
-            ctx.drawImage(spriteSheet, s.x, s.y, s.w, s.h, this.x - cameraX, this.y, this.w, this.h);
-        }
+        if (this.collected) return;
+        const coords = SPRITES.tiles.coin;
+        ctx.drawImage(images.tiles, coords[0] * 16, coords[1] * 16, 16, 16, this.x - cameraX, this.y, this.w, this.h);
     }
 }
 
@@ -207,30 +274,21 @@ function init() {
     
     platforms = [];
     // 地面
-    for(let i=0; i<30; i++) {
-        platforms.push(new GameObject(i * 40, 440, 40, 40, 'ground'));
-    }
+    platforms.push(new Tile(0, 440, 1000, 40, 'ground'));
+    platforms.push(new Tile(1100, 440, 1500, 40, 'ground'));
     
-    // 砖块和障碍
-    platforms.push(new GameObject(200, 320, 40, 40, 'brick'));
-    platforms.push(new GameObject(240, 320, 40, 40, 'question'));
-    platforms.push(new GameObject(280, 320, 40, 40, 'brick'));
+    // 砖块
+    platforms.push(new Tile(200, 320, 120, 40, 'brick'));
+    platforms.push(new Tile(400, 240, 120, 40, 'brick'));
     
-    platforms.push(new GameObject(400, 240, 120, 40, 'brick'));
-    
-    // 水管 (由四部分组成)
-    platforms.push(new GameObject(600, 400, 40, 40, 'pipe_top_left'));
-    platforms.push(new GameObject(640, 400, 40, 40, 'pipe_top_right'));
-    
-    platforms.push(new GameObject(900, 360, 40, 40, 'pipe_top_left'));
-    platforms.push(new GameObject(940, 360, 40, 40, 'pipe_top_right'));
-    platforms.push(new GameObject(900, 400, 40, 40, 'pipe_left'));
-    platforms.push(new GameObject(940, 400, 40, 40, 'pipe_right'));
+    // 水管 (宽80，高80)
+    platforms.push(new Tile(600, 360, 80, 80, 'pipe'));
+    platforms.push(new Tile(900, 360, 80, 80, 'pipe'));
     
     coins = [
         new Coin(250, 280),
         new Coin(450, 200),
-        new Coin(630, 360),
+        new Coin(630, 330),
         new Coin(1200, 400)
     ];
     
@@ -245,52 +303,24 @@ function init() {
 function gameLoop() {
     if (gameOver) return;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    // 背景色
-    ctx.fillStyle = '#5c94fc'; // 经典马里奥蓝天
+    ctx.fillStyle = '#5c94fc'; // 经典马里奥蓝天背景色
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+    
+    // 更新
     player.update();
     
     if (player.x > CONFIG.canvasWidth / 2) {
         cameraX = player.x - CONFIG.canvasWidth / 2;
     }
 
+    // 碰撞检测与绘制
     platforms.forEach(p => {
         const side = checkCollision(player, p);
         if (side === 'top') {
             player.vy = 0;
             player.grounded = true;
         } else if (side === 'bottom') {
-            player.vy *= -1;
-        } else if (side === 'let, CONFIG.tileWidth, CONFIG.tileHeight, 'pipe_right'));
-    }
-}
-
-function gameLoop() {
-    if (gameOver) return;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    // 背景
-    ctx.fillStyle = '#5c94fc';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    player.update();
-    
-    // 相机跟随
-    if (player.x > CONFIG.canvasWidth / 2) {
-        cameraX = player.x - CONFIG.canvasWidth / 2;
-    }
-
-    platforms.forEach(p => {
-        const side = checkCollision(player, p);
-        if (side === 'top') {
             player.vy = 0;
-            player.grounded = true;
-        } else if (side === 'bottom') {
-            player.vy = 2; // 被弹下来
         } else if (side === 'left' || side === 'right') {
             player.vx = 0;
         }
@@ -302,11 +332,11 @@ function gameLoop() {
         const side = checkCollision(player, e);
         if (side === 'top') {
             enemies.splice(idx, 1);
-            player.vy = -12;
+            player.vy = -10;
             score += 100;
             scoreEl.innerText = score;
         } else if (side) {
-            endGame('马里奥阵亡了');
+            endGame('游戏结束');
         }
         e.draw();
     });
@@ -322,8 +352,8 @@ function gameLoop() {
 
     player.draw();
 
-    if (player.x > 3000) {
-        endGame('通关成功！', true);
+    if (player.x > 2500) {
+        endGame('通关大吉！', true);
     }
 
     requestAnimationFrame(gameLoop);
@@ -339,4 +369,39 @@ function checkCollision(obj1, obj2) {
         let overlapX = combinedHalfWidths - Math.abs(dx);
         let overlapY = combinedHalfHeights - Math.abs(dy);
 
-       
+        if (overlapX >= overlapY) {
+            if (dy > 0) {
+                obj1.y += overlapY;
+                return 'bottom';
+            } else {
+                obj1.y -= overlapY;
+                return 'top';
+            }
+        } else {
+            if (dx > 0) {
+                obj1.x += overlapX;
+                return 'left';
+            } else {
+                obj1.x -= overlapX;
+                return 'right';
+            }
+        }
+    }
+    return null;
+}
+
+function endGame(text, win = false) {
+    gameOver = true;
+    statusText.innerText = text;
+    statusText.style.color = win ? '#4ade80' : '#ef4444';
+    overlay.classList.remove('hidden');
+}
+
+function resetGame() {
+    init();
+}
+
+// 等待资源加载后启动
+loadAssets(() => {
+    init();
+});
